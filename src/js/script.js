8e0ef8a59ada043c97bfacef6f6f820c88d2c244
Maqel <98158452+Maqel@users.noreply.github.com>
2022-05-09 22:17:29 +0200

    Add_Class_MyBooks

--- src/js/script.js
+++ src/js/script.js
@@ -9,29 +9,35 @@
       bookList: '.books-list'
     },
   };
-  /*[DONE]Compile the template and assign it to a variable*/
-  const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.bookProduct).innerHTML),
-  };
-  //console.log('templates', templates);
-  /*[DONE]Add a new render function*/
-  const render = function(){
-    /*[DONE]Iterate through all elements of dataSource*/
-    for(let book of dataSource.books) {      
-      const thisBook = this;
-      /*[DONE]Generating HTML code based on the template*/
-      const generatedHTML = templates.menuProduct(book);
-      /*[DONE]Generating an DOM element from HTML*/
-      thisBook.book = utils.createDOMFromHTML(generatedHTML);
-      /*[DONE]Search for list items*/
-      const bookShelf = document.querySelector(select.containerOf.bookList);
-      //console.log('bookShelf:', bookShelf);
-      /*[DONE]Append the generated DOM element as a new DOM child to the .books-list*/
-      bookShelf.appendChild(thisBook.book);
-      //console.log('book:', book);
-    }   
-  };
+  class MyBooks {
+    constructor(){
+      this.render();
+    }
+    /*[DONE]Add a new render function*/
+    render(){
+      /*[START LOOP:][DONE]Iterate through all elements of dataSource*/
+      for(let book of dataSource.books) {      
+        const thisBook = this;
+        /*[DONE]Compile the template and assign it to a variable*/
+        const templates = {
+          menuProduct: Handlebars.compile(document.querySelector(select.templateOf.bookProduct).innerHTML),
+        };
+        //console.log('templates', templates);
+        /*[DONE]Generating HTML code based on the template*/
+        const generatedHTML = templates.menuProduct(book);
+        /*[DONE]Generating an DOM element from HTML*/
+        thisBook.book = utils.createDOMFromHTML(generatedHTML);
+        /*[DONE]Search for list items*/
+        const bookShelf = document.querySelector(select.containerOf.bookList);
+        //console.log('bookShelf:', bookShelf);
+        /*[DONE]Append the generated DOM element as a new DOM child to the .books-list*/
+        bookShelf.appendChild(thisBook.book);
+        //console.log('book:', book);
+      } /*[END LOOP][DONE]*/   
+    }
 
-  render();
   
+  
+  }
+  new MyBooks();
 }
