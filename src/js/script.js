e62f94032ee99f981437541a2ab64f8af6224a34
Maqel <98158452+Maqel@users.noreply.github.com>
2022-05-09 21:29:07 +0200

    exercises_01_render_Function

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+{
+  'use strict';
+  /*[DONE]Prepare a reference to the template and the .books-list*/
+  const select = {
+    templateOf: {
+      bookProduct: '#template-book', 
+    },
+    containerOf:{
+      bookList: '.books-list'
+    },
+  };
+  /*[DONE]Compile the template and assign it to a variable*/
+  const templates = {
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.bookProduct).innerHTML),
+  };
+  //console.log('templates', templates);
+  /*[DONE]Add a new render function*/
+  const render = function(){
+    /*[DONE]Iterate through all elements of dataSource*/
+    for(let book of dataSource.books) {      
+      const thisBook = this;
+      /*[DONE]Generating HTML code based on the template*/
+      const generatedHTML = templates.menuProduct(book);
+      /*[DONE]Generating an DOM element from HTML*/
+      thisBook.book = utils.createDOMFromHTML(generatedHTML);
+      /*[DONE]Search for list items*/
+      const bookShelf = document.querySelector(select.containerOf.bookList);
+      //console.log('bookShelf:', bookShelf);
+      /*[DONE]Append the generated DOM element as a new DOM child to the .books-list*/
+      bookShelf.appendChild(thisBook.book);
+      //console.log('book:', book);
+    }   
+  };
+
+  render();
+  
+}
